bddd8f611c81aa9a12267a0ac385cc3f47bafa2c
23-101174-create <23-101174@students.eui.edu.eg>
2025-12-14 19:12:57 +0200

    3d-try

--- code.js
+++ code.js
@@ -4,3 +4,4 @@ document.getElementById("Models") .innerHTML = "Models";
 document.getElementById("About") .innerHTML = "About";
 document.getElementById("Contact") .innerHTML = "Contact";
 document.getElementById("button-1") .innerHTML = "Login";
+document.getElementById("hero-text") .innerHTML = "Mercedes";

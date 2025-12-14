ea68b867b9bd55d143e9717d482b6ae00d66d483
23-101174-create <23-101174@students.eui.edu.eg>
2025-12-14 18:46:18 +0200

    nav

---
+++ code.js
@@ -0,0 +1,6 @@
+document.getElementById("Home").innerHTML = "Home";
+document.getElementById("Gallery") .innerHTML = "Gallery";
+document.getElementById("Models") .innerHTML = "Models";
+document.getElementById("About") .innerHTML = "About";
+document.getElementById("Contact") .innerHTML = "Contact";
+document.getElementById("button-1") .innerHTML = "Login";

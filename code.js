5de933a7a79efa941e8286073af851617831ff98
23-101174-create <23-101174@students.eui.edu.eg>
2025-12-25 04:28:44 +0200

    last-add

--- code.js
+++ code.js
@@ -19,3 +19,47 @@ document.getElementById("ma") .innerHTML = "Maserati GranCabrio";
 document.getElementById("audi") .innerHTML = "Audi SQ8";
 document.getElementById("mer") .innerHTML = "Mercedes-Benz";
 document.getElementById("po") .innerHTML = "Porsche";
+document.getElementById("ex") .innerHTML = "Explore Models";
+document.getElementById("last-title") .innerHTML = "About SilverStar Motors";
+document.getElementById("last-sub") .innerHTML = "Explore high-end cars like never before. At SilverStar Motors, every model is interactive, immersive and crafted to perfection";
+document.getElementById("button-6") .innerHTML = "Explore for more";
+document.getElementById("label-address").innerHTML = "Address:";
+document.getElementById("address-text").innerHTML = "52 Aurora Drive, Skyline District";
+document.getElementById("label-city").innerHTML = "Magnolia City, Nevada, USA";
+document.getElementById("label-email").innerHTML = "Email:";
+document.getElementById("email-text").innerHTML = "support@silverstarmotors.com";
+document.getElementById("label-phone").innerHTML = "Phone:";
+document.getElementById("phone-text").innerHTML = "+1-800-442-9090";
+document.getElementById("footer-bottom-text").innerHTML = "© 2025 SilverStar Motors. All rights reserved";
+document.getElementById("footer-center-title").innerHTML = "SilverStar Motors";
+document.getElementById("nav-home").innerHTML = "Home";
+document.getElementById("nav-gallery").innerHTML = "Gallery";
+document.getElementById("nav-models").innerHTML = "Models";
+document.getElementById("nav-about").innerHTML = "About";
+document.getElementById("nav-contact").innerHTML = "Contact";
+document.getElementById("subscribe-title").innerHTML = "Subscribe";
+document.getElementById("subscribe-description").innerHTML = "Stay updated with the latest car releases, luxury updates, and exclusive offers from SilverStar Motors";
+document.getElementById("subscribe-email-input").placeholder = "Enter your email";
+document.getElementById("subscribe-button").innerHTML = "Submit";
+
+
+const scrollBtn = document.createElement('div');
+scrollBtn.innerHTML = '↑';
+scrollBtn.classList.add('scroll');
+
+document.body.appendChild(scrollBtn);
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollBtn.style.display = 'flex';
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+});
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
